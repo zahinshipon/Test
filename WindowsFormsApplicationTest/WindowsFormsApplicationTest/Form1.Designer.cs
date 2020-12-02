d4777e6ced8d809274b3abfbc50cd5c27194627f
ZAHIDLOSHAN <zahidloshan@gmail.com>
2020-12-03 01:01:59 +0600

    2nd commit

--- WindowsFormsApplicationTest/WindowsFormsApplicationTest/Form1.Designer.cs
+++ WindowsFormsApplicationTest/WindowsFormsApplicationTest/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -50,11 +51,21 @@
             this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 1;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(327, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(460, 318);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -68,6 +79,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- WindowsFormsApplicationTest/WindowsFormsApplicationTest/Form1.cs
+++ WindowsFormsApplicationTest/WindowsFormsApplicationTest/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApplicationTest
 
         private void button1_Click(object sender, EventArgs e)
         {
-            label1.Text = "Show";
+            label2.Text = "Hi This is first code";
         }
     }
 }
